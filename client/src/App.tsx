55d4792521b23b5ff669d1e58209b8bb8515e860
Adir Noyman <ofahol2007@hotmail.com>
2025-10-11 15:23:45 +0300

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-
-  const [activities, setActivities] = useState<string[]>([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch('https://localhost:5001/api/activities')
-      .then(response => response.json())
-      .then(data => setActivities(data))
-      .catch(error => console.error('Error fetching activities:', error));
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => {
+        setActivities(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching activities:', error);
+      });
+      
   }, []);
 
-
-  return <><h1 className="app">Adiros Activities 🤓👇</h1>
-  <ul>
-    {activities.map((activity) => (
-      <li key={activity.id}>{activity.title}</li>
-    ))}
-  </ul>
-  </>;
+  return (
+    <>
+      <Typography variant='h2' component='h1' gutterBottom>
+        Adiros Activities 🤓👇
+      </Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
 }
 
 export default App;
